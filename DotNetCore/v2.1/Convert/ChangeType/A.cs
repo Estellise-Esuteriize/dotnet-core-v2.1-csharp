4c1ceb9f03f5e4009a45c3c34bc9a1ec7433cfb0
Estellise <shiranoyukihime@gmail.com>
2020-01-13 22:48:01 +0800

    Initial commit

---
+++ DotNetCore/v2.1/Convert/ChangeType/A.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetCore.v2._1.Convert.ChangeType
+{
+    public class A
+    {
+        public virtual string Message()
+        {
+            return "This is From A";
+        }
+    }
+}
---
+++ DotNetCore/v2.1/Convert/ChangeType/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DotNetCore.v2._1.Convert.ChangeType
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            A a = new A();
+            B b = new B();
+            
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(b.GetType());
+
+            var ba = (A) System.Convert.ChangeType(b, typeof(A));
+            
+            Console.WriteLine(ba.Message());
+
+        }
+    }
+}
